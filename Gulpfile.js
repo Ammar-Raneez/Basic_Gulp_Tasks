800e7b5c0e824ee37b468adc3b80f57cb767cb86
ammar <ammarraneez@gmail.com>
2022-07-23 15:45:27 +0530

    feat: copy html task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,23 @@
-const gulp = require('gulp');
 // top level functions
 // gulp.task = Define tasks
 // gulp.src = points to files to use
 // gulp.dest = points to folder to output
 // gulp.watch = watch files and folders for changes
 
+import gulp from 'gulp';
+
 // logs message
 gulp.task('message', async function () {
   return console.log('Gulp is running...')
-})
+});
 
 // Default task - doesnt require specifying task name on command - gulp {default}
 gulp.task('default', async function () {
   return console.log('Gulp is running...')
-})
+});
+
+// Copy html files
+gulp.task('copy-html', async function () {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
